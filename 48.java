9b0f8f1dff19b2e56bbacc89ac929b4f3442d3b5
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-23 09:46:55 +0530

    Update 48.java

--- 48.java
+++ 48.java
@@ -1,3 +1,5 @@
+/* package whatever; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -10,11 +12,16 @@ class Ideone
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt();
 		int fa[]=new int[a];int j=0;
+		boolean st=false;
 		for(int i=2;i<a;i++)
 		{
 			if(a%i==0 && i%2==1)
+			{
 			fa[j++]=i;
+			st=true;
+			}
 		}
+		if(st)
 		System.out.print(fa[0]);
 		for(int i=1;i<j;i++)
 		System.out.print(" "+fa[i]);
